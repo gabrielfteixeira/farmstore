2bb5593fdf57ef55abe864fef5068cc084403c55
Gabriel <gabriel.teixeira009@gmail.com>
2025-02-23 14:02:02 -0300

    feature: Adicionando arquivos

---
+++ src/test/java/com/farmstore/Farmstore/FarmStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmstore.Farmstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
