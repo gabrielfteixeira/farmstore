8f2d08206ef75eacb0b141e918cf5ded462fb423
Gabriel <gabriel.teixeira009@gmail.com>
2025-03-27 22:59:27 -0300

    feat: use case para cadastro de fornecedores

---
+++ src/main/java/com/farmstore/Farmstore/core/usecases/fornecedor/CadastrarFornecedorUseCase.java
@@ -0,0 +1,8 @@
+package com.farmstore.Farmstore.core.usecases.fornecedor;
+
+import com.farmstore.Farmstore.core.entity.Fornecedor;
+
+public interface CadastrarFornecedorUseCase {
+
+    Fornecedor execute(Fornecedor fornecedor);
+}
---
+++ src/main/java/com/farmstore/Farmstore/core/usecases/fornecedor/CadastrarFornecedorUseCaseImpl.java
@@ -0,0 +1,11 @@
+package com.farmstore.Farmstore.core.usecases.fornecedor;
+
+import com.farmstore.Farmstore.core.entity.Fornecedor;
+
+public class CadastrarFornecedorUseCaseImpl implements CadastrarFornecedorUseCase{
+
+    @Override
+    public Fornecedor execute(Fornecedor fornecedor) {
+        return null;
+    }
+}
